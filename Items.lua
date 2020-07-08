92be5f71aeb68890669605a7e4a79065601eb3f4
TheKingElessar <wasabimath@gmail.com>
2020-07-08 01:07:54 -0600

    latest version

--- Items.lua
+++ Items.lua
@@ -1,6 +1,8 @@
 health = 50
 local WorldAPI = require("WorldAPI")
 
+-- Test to see how fast it updates bro
+
 function Scream()
 	local sounds = {"quark:entity.stoneling.die"}
 	local key = math.random(1, 1)
