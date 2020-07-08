b9640d7248a01d33542ea2dd54c8e75c7987d105
TheKingElessar <wasabimath@gmail.com>
2020-07-07 18:24:37 -0600

    github

--- Update.lua
+++ Update.lua
@@ -2,9 +2,11 @@ shell.run("delete", "live")
 shell.run("delete", "WorldAPI")
 shell.run("delete", "MovementAPI")
 shell.run("delete", "ItemsAPI")
+shell.run("delete", "Update")
 
 shell.run("wget", "https://raw.githubusercontent.com/TheKingElessar/CC-Programs/master/World.lua", "WorldAPI")
 shell.run("wget", "https://raw.githubusercontent.com/TheKingElessar/CC-Programs/master/Movement.lua", "MovementAPI")
 shell.run("wget", "https://raw.githubusercontent.com/TheKingElessar/CC-Programs/master/Items.lua", "ItemsAPI")
+shell.run("wget", "https://raw.githubusercontent.com/TheKingElessar/CC-Programs/master/Update.lua", "Update")
 
 shell.run("wget", "https://raw.githubusercontent.com/TheKingElessar/CC-Programs/master/Live.lua", "live")
