05fe4e934843c117adada46b999873695b9e7152
TheKingElessar <wasabimath@gmail.com>
2020-07-07 18:23:39 -0600

    changed to github

--- Update.lua
+++ Update.lua
@@ -3,8 +3,8 @@ shell.run("delete", "WorldAPI")
 shell.run("delete", "MovementAPI")
 shell.run("delete", "ItemsAPI")
 
-shell.run("pastebin", "get", "SBbhKTm7", "WorldAPI")
-shell.run("pastebin", "get", "u43kCibn", "MovementAPI")
-shell.run("pastebin", "get", "3KHx4N2T", "ItemsAPI") -- Gotta update the codes here
+shell.run("wget", "https://raw.githubusercontent.com/TheKingElessar/CC-Programs/master/World.lua", "WorldAPI")
+shell.run("wget", "https://raw.githubusercontent.com/TheKingElessar/CC-Programs/master/Movement.lua", "MovementAPI")
+shell.run("wget", "https://raw.githubusercontent.com/TheKingElessar/CC-Programs/master/Items.lua", "ItemsAPI")
 
-shell.run("pastebin", "get", "WMkF82me", "live")
+shell.run("wget", "https://raw.githubusercontent.com/TheKingElessar/CC-Programs/master/Live.lua", "live")
