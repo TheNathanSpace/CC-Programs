8b4ab651c6672acd38d3c77a4d7eaa5e06c0db94
TheKingElessar <wasabimath@gmail.com>
2020-07-30 13:40:55 -0600

    latest version

--- Client/Mapping.lua
+++ Client/Mapping.lua
@@ -1,4 +1,4 @@
--- 0.4.0
+-- 0.4.1
 
 local Util = require("Util")
 local World = require("World")
@@ -38,73 +38,84 @@ function addCurrentLocation()
 	traversedOpenSpaces[currentLocationKey] = ""
 end
 
--- Start loop. Still gotta figure this out
+function doMapping()
+	-- Start loop. Still gotta figure this out
 
-currentX, startY, currentZ = World.returnLocation()
-currentFacing = World.getFacing()
-currentlyMapping = true
-
-if currentX == nil then
-	turtle.forward()
-	World.getLocation()
 	currentX, startY, currentZ = World.returnLocation()
 	currentFacing = World.getFacing()
-end
+	currentlyMapping = true
 
-while not (currentFacing == 4) do
-	turnRight()
-end
+	if currentX == nil then
+		turtle.forward()
+		World.getLocation()
+		currentX, startY, currentZ = World.returnLocation()
+		currentFacing = World.getFacing()
+	end
 
-while true do
-	addCurrentLocation()
-	
-	-- Check left
-	turnLeft()
-	local leftClear = not turtle.detect()
-	if leftClear then
-		local facingX, facingY, facingZ = World.getFacingBlock()
-		local facingLocationKey = Util.createLocationKey(facingX, facingZ)
-		uncheckedOpenSpaces[facingLocationKey] = ""
+	while not (currentFacing == 4) do
+		turnRight()
 	end
-	
-	-- Check right
-	turnRight()
-	turnRight()
-	local rightClear = not turtle.detect()
-	if rightClear then
-		local facingX, facingY, facingZ = World.getFacingBlock()
-		local facingLocationKey = Util.createLocationKey(facingX, facingZ)
-		uncheckedOpenSpaces[facingLocationKey] = ""
-	end
-	
-	turnLeft() -- Reset to straight
-	
-	-- Check front
-	local frontClear = not turtle.detect()
-	
-	if not frontClear then
+
+	while true do
+		addCurrentLocation()
+		
+		-- Check left
+		turnLeft()
+		local leftClear = not turtle.detect()
 		if leftClear then
-			turnLeft()
-			turtle.forward()
-			turnLeft()
-			addCurrentLocation()
-		else if rightClear then
-			turnRight()
-			turtle.forward()
-			turnRight()
-			addCurrentLocation()
-		else
-			local currentLocationKey = Util.createLocationKey(currentX, currentZ)
-			local result = Pathfinding.processLocations(currentLocationKey, Util.getFirstKey(uncheckedOpenSpaces), traversedOpenSpaces)
-			
-			while not (currentFacing == 4) do
+			local facingX, facingY, facingZ = World.getFacingBlock()
+			local facingLocationKey = Util.createLocationKey(facingX, facingZ)
+			uncheckedOpenSpaces[facingLocationKey] = ""
+		end
+		
+		-- Check right
+		turnRight()
+		turnRight()
+		local rightClear = not turtle.detect()
+		if rightClear then
+			local facingX, facingY, facingZ = World.getFacingBlock()
+			local facingLocationKey = Util.createLocationKey(facingX, facingZ)
+			uncheckedOpenSpaces[facingLocationKey] = ""
+		end
+		
+		turnLeft() -- Reset to straight
+		
+		-- Check front
+		local frontClear = not turtle.detect()
+		
+		if not frontClear then
+			if leftClear then
+				turnLeft()
+				turtle.forward()
+				turnLeft()
+				addCurrentLocation()
+			else if rightClear then
 				turnRight()
+				turtle.forward()
+				turnRight()
+				addCurrentLocation()
+			else
+				local currentLocationKey = Util.createLocationKey(currentX, currentZ)
+				local keyExists = Util.getFirstKey(uncheckedOpenSpaces)
+				
+				if not (keyExists == nil) then
+					local result = Pathfinding.processLocations(currentLocationKey, keyExists, traversedOpenSpaces)
+				
+					while not (currentFacing == 4) do
+						turnRight()
+					end
+				else
+					break
+				end
 			end
 		end
+		-- Should make sure aligned to where you wanna go
+		
+		turtle.forward()
 	end
-	-- Should make sure aligned to where you wanna go
+	print("Yay")
+	currentlyMapping = false
 	
-	turtle.forward()
 end
 
-return {getCurrentlyMapping = getCurrentlyMapping, setCurrentFacing = setCurrentFacing}
+return {getCurrentlyMapping = getCurrentlyMapping, setCurrentFacing = setCurrentFacing, doMapping = doMapping}
