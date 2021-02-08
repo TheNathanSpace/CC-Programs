08bba33db9edb0453450b76e5edb45a0f35ccb8c
TheKingElessar <wasabimath@gmail.com>
2021-02-08 15:57:17 -0700

    latest version

--- Horton/Main.lua
+++ Horton/Main.lua
@@ -1,4 +1,4 @@
--- 0.1.3
+-- 0.1.4
 
 local Location = require("Location")
 
@@ -24,7 +24,8 @@ end
 
 function Tick()
 	Location.getLocation()
-	Reset()
+	writeScreen()
+  Reset()
 end
 
 while true do
